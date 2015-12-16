a6984f39ec84eda5f9750df691863da0bc67d977
alex <supermvn@tom.com>
2015-12-16 11:52:50 +0800

    初始一个项目

---
+++ StartUpProject/StartUpProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StartUpProject
+//
+//  Created by Mi on 15/12/16.
+//  Copyright © 2015年 thinkjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

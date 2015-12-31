87f9a38c607bb813cd0c662f80f8f86f615f8dc7
alex <supermvn@tom.com>
2015-12-31 16:05:08 +0800

    init

---
+++ StartUpProject/Pods/Target Support Files/Pods-StartUpProject/Pods-StartUpProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StartUpProject : NSObject
+@end
+@implementation PodsDummy_Pods_StartUpProject
+@end
---
+++ StartUpProject/Pods/Target Support Files/Pods-StartUpProjectTests/Pods-StartUpProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StartUpProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_StartUpProjectTests
+@end
